6b61d84af275bfd1f1fb12dc728a4ad0e3b25869
IanStafford <ianstafford@ufl.edu>
2025-03-31 13:39:46 -0400

    restructure pt 3

---
+++ OLD/old fieldplate/flatplateTest.tcl
@@ -0,0 +1,24 @@
+pdbSetDouble Nitride DevPsi RelEps 6.9
+pdbSetDouble HighK DevPsi RelEps 35.0
+
+# source run_measurements.tcl
+source test1.tcl
+
+source fieldplate_150nm.tcl
+source GaN_modelfile_masterD
+
+test1 100 flatplate
+
+if {0} {
+
+window row=1 col=2
+
+run_measurements "flatplate.csv" flatplate
+
+source flatplate_1000.tcl
+source GaN_modelfile_masterD
+
+run_measurements "flatplate1000.csv" flatplate1000
+
+window row=1 col=1
+}
