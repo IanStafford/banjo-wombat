4f6d74f969b70526e989a7fde16a9cb90c0b9677
IanStafford <ianstafford@ufl.edu>
2025-03-31 13:38:11 -0400

    restructure pt 1
    Moves all the old fieldplate stuff to an OLD folder, adds some screenshots

--- fieldplate/justFieldpeak.tcl
+++
@@ -1,8 +0,0 @@
-source plotPeak.tcl
-window row=1 col=1
-
-source fieldplate_150nm.tcl
-plotPeak "peak_150nm.csv" 100 150nm
-plotPeak "fieldplate_100nm.tcl" "peak_100nm.csv" 100 100nm
-plotPeak "fieldplate_50nm.tcl" "peak_50nm.csv" 100 50nm
-plotPeak "fieldplate_10nm.tcl" "peak_10nm.csv" 100 10nm
