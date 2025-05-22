d10d22fa0460bf750a69d53c841cb344d38b264c
Ian Stafford <ianstafford@ufl.edu>
2025-05-22 15:17:25 -0400

    May 22 Commit

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 I compiled my flooxs decks into this github as a way to access them from my macbook and my linux desktop.
 
 It should contain (as of 03/06/2025) files for the fieldplate and T gate structure.
+
+# Research Updates
+Major milestones will now be recorded in the readme as of 5/22/2025
+___
+## Experimental Data Recieved (5/22)
+Experimental data from Dr. Anderson is recieved and plotted against our simulations. The simulations need some tweaking, and future additions to the devices include a layer of oxide between the gate and the AlGaN, slope tuning, threshold voltage tuning, and more. Currently roadblocks are an incomplete understanding of the microstructure and an accumulation of holes at the gate contact as a result of high doping (questionable).
+___
--- allNit/fieldpeak.tcl
+++ allNit/fieldpeak.tcl
@@ -1,23 +1,32 @@
 source run_measurements.tcl
+window row=1 col=1
 
+if {0} {
 source tgate_mod.tcl
 source GaN_modelfile_masterD
-pdbSetDouble Nitride DevPsi RelEps 6.3
 Initialize
 device init
 contact name=D supply=0.1
-window row=1 col=1
 
 run_measurements "tgate_Vt.csv" "tgate_peakField.csv" tgate
+}
 
 if {0} {
-source fieldplate_matchedGate.tcl
+source fieldplate_mod.tcl
 source GaN_modelfile_masterD
-pdbSetDouble Nitride DevPsi RelEps 6.3
 Initialize
 device
 contact name=D supply=0.1
 
-
 run_measurements "fieldplate_Vt.csv" "fieldplate_peakField.csv" fieldplate
+}
+
+if {1} {
+source fieldplate_ox.tcl
+source GaN_modelfile_masterD
+Initialize
+device
+contact name=D supply=0.1
+
+run_measurements "fieldplate_ox_Vt.csv" "fieldplate_ox_peakField.csv" fieldplate_ox
 }
