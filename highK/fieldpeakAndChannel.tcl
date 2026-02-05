b236b72409b034520561a1bc4b7433612fb3da33
Ian Stafford <ianstafford@ufl.edu>
2026-02-05 11:19:29 -0500

    Add new fieldpeakAndChannel.tcl script for highK simulations
    Going home for weekend and need everything local

--- highK/fieldpeakAndChannel.tcl
+++ highK/fieldpeakAndChannel.tcl
@@ -1,52 +1,65 @@
-source fieldplate.tcl
+source fieldplate_highk.tcl
 source GaN_modelfile_masterD
 pdbSetDouble Nitride DevPsi RelEps 6.3
 pdbSetDouble HighK DevPsi RelEps 35.0
+
 Initialize
 device init
-contact name=D supply=0.1
-device
+
 window row=1 col=3
 
 
-# Loop over Vds values from 0.0 to 50.0 (approximately)
-set g -4.0
-set vdsMAX 100.0
-contact name=G supply=$g
+set vdsMAX 50.0
 
+# Bring gate down to -4.0V
+for {set g 0.0} {$g > -4.0} {set g [expr $g-0.25]} {
+    contact name=G supply=$g
+    device
+}
+
+# Loop over Vds values from 0.0 to 50.0 (approximately)
 for {set vds 0.0} {$vds < [expr $vdsMAX + 0.1]} {set vds [expr $vds+0.25]} {
     contact name=D supply=$vds
     device
-    sel z=abs(dot(DevPsi,y))*1.0e-4         
+    sel z=abs(dot(DevPsi,y))*1.0e-4    
+    # plot2d levels=20 xmax=0.5 <-- uncomment to see the field distribution
     set pstr [peak GaN]
     puts [peak GaN]
     set peakfield [lindex $pstr 1]
     
-    chart graph=PeakField curve=Fieldplate xval=$vds yval=$peakfield leg.left ylab= "Electric Field (V/cm)" title= "Peak Field"
+    chart graph=PeakField curve=HighK xval=$vds yval=$peakfield leg.left ylab= "Electric Field (V/cm)" title= "Peak Field"
     if { [expr {$vds - floor($vds/25)*25}] == 0 } {
         plot1d graph=Channel xv=0.018 ylab= "Electric Field (V/cm)" title= "Channel Field" name = "Vds = $vds"
     }
 }
 
 
-source fp_HighK.tcl
+source fieldplate_highk.tcl
 source GaN_modelfile_masterD
 pdbSetDouble Nitride DevPsi RelEps 6.3
-pdbSetDouble HighK DevPsi RelEps 35.0
+pdbSetDouble HighK DevPsi RelEps 6.3
+
 Initialize
 device init
-contact name=D supply=0.1
-device
 
+# Bring gate down to -4.0V
+for {set g 0.0} {$g > -4.0} {set g [expr $g-0.25]} {
+    contact name=G supply=$g
+    device
+}
+
+# Loop over Vds values from 0.0 to 50.0 (approximately)
 for {set vds 0.0} {$vds < [expr $vdsMAX + 0.1]} {set vds [expr $vds+0.25]} {
     contact name=D supply=$vds
     device
+
+    # Calculate the peak electric field in the GaN layer and plot it against Vds
     sel z=abs(dot(DevPsi,y))*1.0e-4         
     set pstr [peak GaN]
     puts [peak GaN]
     set peakfield [lindex $pstr 1]
     
-    chart graph=PeakField curve=Fieldplate_HighK xval=$vds yval=$peakfield leg.left ylab= "Electric Field (V/cm)" title= "Peak Field"
+    chart graph=PeakField curve=SiN xval=$vds yval=$peakfield leg.left ylab= "Electric Field (V/cm)" title= "Peak Field"
     if { [expr {$vds - floor($vds/25)*25}] == 0 } {
         plot1d graph=Channel2 xv=0.018 ylab= "Electric Field (V/cm)" title= "Channel Field (With 50nm Nitride Layer)" name = "Vds = $vds"
     }
